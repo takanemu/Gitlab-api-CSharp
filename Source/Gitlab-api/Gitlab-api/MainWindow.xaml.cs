dccf0598e958bc9492c3986af3f8d4128c8bb5e5
澁谷 高礼 <takanori_shibuya@2fc-group.jp>
2012-11-30 20:35:42 +0900

    JSONのパース実験中

--- Source/Gitlab-api/Gitlab-api/MainWindow.xaml.cs
+++ Source/Gitlab-api/Gitlab-api/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Gitlab.Api.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using Codeplex.Data;
 
 namespace Gitlab_api
 {
@@ -23,6 +29,42 @@ namespace Gitlab_api
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void TestButtonClickHandler(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                HttpResponseMessage response = await client.GetAsync("http://192.168.11.47/api/v2/projects?private_token=5Bn5tKi3qsB87nwcKzNB");
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var projects = DynamicJson.Parse(responseBody, System.Text.UTF8Encoding.UTF8);
+
+                foreach (var item in projects)
+                {
+
+
+                    Console.WriteLine("id:" + item["id"]);
+                    Console.WriteLine("code:" + item.code);
+                    Console.WriteLine("name:" + item.name);
+                    //Console.WriteLine("description:" + item.description);
+                    //Console.WriteLine("path:" + item.path);
+                    //Console.WriteLine("default_branch:" + item.default_branch);
+                    
+                        
+                        
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", ex.Message);
+            }
         }
     }
 }
---
+++ Source/Gitlab-api/Gitlab.Api.Library/Projects.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Gitlab.Api.Library
+{
+    [DataContract]
+    public class Projects
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+    }
+}
